eb1926e796a3d62cf5f48342f85fafecedadc0d9
Hemershon Silva <hemershon@gmail.com>
2019-02-22 13:14:59 -0300

    update

--- js/agency.js
+++ js/agency.js
@@ -74,3 +74,32 @@ function animation() {
   {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
   title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
 }
+jQuery.fn.toggleText = function(a,b) {
+
+  return   this.html(this.html().replace(new RegExp("("+a+"|"+b+")"),function(x){return(x==a)?b:a;}));
+  
+  }
+  
+  $(document).ready(function(){
+  
+  $('.tgl').before('<span><font color=red>more »</font></span>');
+  
+  $('.tgl').css('display', 'none')
+  
+  $('span', '#box-toggle').click(function() {
+  
+  $(this).next().slideToggle('slow')
+  
+  .siblings('.tgl:visible').slideToggle('fast');
+  
+  // aqui começa o funcionamento do plugin
+  
+  $(this).toggleText('more »','« less')
+  
+  .siblings('span').next('.tgl:visible').prev()
+  
+  .toggleText('more »','« less')
+  
+  });
+  
+  })
